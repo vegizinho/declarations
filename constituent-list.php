64d05fc6d66af6b632418ec5712498976bf06bff
vegizinho <miroslav.vegh@techlab.hr>
2014-09-06 09:20:17 +0200

    Declarations, initial commit

---
+++ constituent-list.php
@@ -0,0 +1,365 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Carousel Template for Bootstrap v1</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="index.php">Milki deklaracije</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="declaration-list.php">Lista deklaracija</a></li>
+                <li><a href="constituent-list.php">Lista konstituenata</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Radnje <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="declaration-create.php">Kreiraj deklaraciju</a></li>
+                    <li><a href="declaration-edit.php">Editiraj deklaraciju</a></li>
+                    <li><a href="declaration-disable.php">Obrisi deklaraciju</a></li>
+                    <li><a href="constituent-create.php">Kreiraj konstituent</a></li>
+                    <li><a href="constituent-edit.php">Editiraj konstituent</a></li>
+                    <li><a href="constituend-disable.php">Obrisi konstituent</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Dodatne opcije</li>
+                    <li><a href="printing-options.php">Print opcije</a></li>
+                    <li><a href="ask-for-help.php">Zatrazi pomoc!</a></li>
+                  </ul>
+                  <li><a href="how-tos.php">Uputstva</a></li>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+    
+    <hr class="featurette-divider">
+
+    <div class="container">
+
+	<div class="row">
+		<div class="col-md-8">
+		<h2>Logotip</h2>
+		</div>
+		<div class="col-md-4">
+		Specifikacije/datum/stranica
+		</div>
+	</div>
+	
+	
+	<div class="row">
+		<div class="col-md-2">
+		Tekst 1
+		</div>
+		<div class="col-md-2">
+		Tekst 2
+		</div>
+		<div class="col-md-2">
+		Tekst 3
+		</div>
+		<div class="col-md-3">
+		Tekst 4
+		</div>
+		<div class="col-md-3">
+		Tekst 5
+		</div>
+	</div>
+	
+	<hr>
+	
+	<div class="row">
+		<div class="col-md-6">
+		<strong>Name</strong>
+		</div>
+		<div class="col-md-2">
+		<strong>Unit</strong>
+		</div>
+		<div class="col-md-2">
+		<strong>Premix</strong>
+		</div>
+		<div class="col-md-2">
+		<strong>End product</strong>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		Vitamin A (E672)
+		</div>
+		<div class="col-md-2">
+		IU
+		</div>
+		<div class="col-md-2">
+		300000.00
+		</div>
+		<div class="col-md-2">
+		22500.00
+		</div>
+	</div>
+	
+    
+    
+
+
+	<hr>
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+    <script src="bootstrap-3.2.0-dist/assets/js/docs.min.js"></script>
+  </body>
+</html>
